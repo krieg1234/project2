0db7adfb6a11c44fa2877f8d8f012ebf02127029
krieg1234 <olegkrieg@gmail.com>
2022-01-12 17:57:47 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Title: React.FC = () => <h1>Hello, world!</h1>;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Title />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
